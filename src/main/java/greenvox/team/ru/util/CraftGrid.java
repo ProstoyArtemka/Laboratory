e2418092762de87352f1938e65ec10b5e17b48d2
Unillion <114824496+Unillion@users.noreply.github.com>
2023-02-11 14:05:45 +0500

    CraftGrid (Don't kill me pls)

--- src/main/java/greenvox/team/ru/util/CraftGrid.java
+++ src/main/java/greenvox/team/ru/util/CraftGrid.java
@@ -1,24 +1,52 @@
 package greenvox.team.ru.util;
 
 import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-import javax.xml.stream.Location;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CraftGrid implements Listener {
-    public CraftGrid(Location location, ArrayList<ItemStack> craft) {
+    private static final ArrayList<ItemStack> StacksInFrames = new ArrayList<>();
 
+    public CraftGrid(Location location, ArrayList<ItemStack> craft) {
+        Entity entities = (Entity) location.toCenterLocation().getBlock().getBoundingBox().expand(-1,0,-1,1,1,1);
+
+        getStackInFrame(location, entities);
+
+        containsCraft(craft);
     }
 
-    public static boolean containsCraft() {
+    public static boolean containsCraft(ArrayList<ItemStack> craft) {
+        if (StacksInFrames.equals(craft)) return true;
+
         return false;
     }
 
     @EventHandler
     public void onItemPutInFrame(PlayerItemFrameChangeEvent e) {
-        // Do glow for correct items for craft
+        //later :cry:
+    }
+
+    private static void getStackInFrame(Location location, Entity nearEntities) {
+        if (!(location.toCenterLocation().getNearbyEntities(0,1,0) instanceof ItemFrame)) return;
+
+        if (location.toCenterLocation().getBlock().getBoundingBox().expand(-1,0,-1,1,1,1) instanceof ItemFrame) {
+
+            List<ItemFrame> frameList = new ArrayList<>();
+            frameList.add((ItemFrame) nearEntities);
+
+            for (ItemFrame entity : frameList) {
+
+                ItemStack itemStackInFrame = entity.getItem();
+
+                CraftGrid.StacksInFrames.add(itemStackInFrame);
+            }
+        }
     }
 }
